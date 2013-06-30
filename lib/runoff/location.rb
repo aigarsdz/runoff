24c9fd9c79d17994970d4415d6342fc66ddaa797
Aigars Dzerviniks <dzerviniks.aigars@outlook.com>
2013-06-30 10:24:13 +0300

    Add Windows 8 Skype support

--- lib/runoff/location.rb
+++ lib/runoff/location.rb
@@ -28,6 +28,10 @@ module Runoff
       if RbConfig::CONFIG['host_os'] =~ /mingw/
         location = "#{ENV['APPDATA']}\\Skype\\#{skype_username}\\main.db"
 
+        unless File.exist?("#{ENV['APPDATA']}\\Skype")
+          location = self.get_default_skype_data_location_on_windows_8 skype_username
+        end
+
         location.gsub!(/\\/, '/')
         location.gsub(/^[a-zA-Z]:/, '')
       elsif RbConfig::CONFIG['host_os'] =~ /linux/
@@ -35,6 +39,31 @@ module Runoff
       else
         "#{ENV['HOME']}/Library/Application Support/Skype/#{skype_username}/main.db"
       end
+    end
+
+    private
+
+    # Public: Composes the default Skype database location for the Windows 8 operating system.
+    #
+    # skype_username - A String that contains a username of the Skype account,
+    #                  which database we want to access.
+    #
+    # Examples
+    #
+    #   get_default_skype_data_location_on_windows_8 skype_username
+    #   # => C:/Users/user/AppData/Local/Packages/Microsoft.SkypeApp_sakjhds8asd/LocalState/skype_username/main.db
+    #
+    # Returns a String that contains the path to the Skype database file.
+    def self.get_default_skype_data_location_on_windows_8(skype_username)
+      location = "#{ENV['HOME']}/AppData/Local/Packages"
+
+      Dir["#{location}/*"].each do |item|
+        if item =~ /Microsoft\.SkypeApp/
+          location = "#{item}/LocalState/#{skype_username}/main.db"
+        end
+      end
+
+      location
     end
   end
 end
--- test/location_test.rb
+++ test/location_test.rb
@@ -3,15 +3,21 @@ require 'minitest/unit'
 require 'runoff'
 
 class TestLocation < MiniTest::Test
-  def test_must_return_a_default_path_depending_on_the_operating_system
-    path = Runoff::Location.default_skype_data_location 'aidzis_skype'
+  def setup
+    @path = Runoff::Location.default_skype_data_location 'aidzis_skype'
+  end
 
+  def test_must_return_a_default_path_depending_on_the_operating_system
     if RbConfig::CONFIG['host_os'] =~ /mingw/
-      assert_match /\/Users\/[a-zA-Z0-9]+\/AppData\/Roaming\/Skype\/aidzis_skype\/main\.db/, path
+      if File.exist?("#{ENV['APPDATA']}\\Skype")
+        assert_match /\/Users\/[a-zA-Z0-9]+\/AppData\/Roaming\/Skype\/aidzis_skype\/main\.db/, @path
+      else
+        assert_match /\/Users\/[a-zA-Z0-9]+\/AppData\/Local\/Packages\/[a-zA-Z0-9_\/\.]+\/aidzis_skype\/main\.db/, @path
+      end
     elsif RbConfig::CONFIG['host_os'] =~ /linux/
-      assert_match /\/home\/[a-zA-Z0-9]+\/\.Skype\/aidzis_skype\/main\.db/, path
+      assert_match /\/home\/[a-zA-Z0-9]+\/\.Skype\/aidzis_skype\/main\.db/, @path
     else
-      assert_match /~\/Library\/Application Support\/Skype\/aidzis_skype\/main\.db/, path
+      assert_match /~\/Library\/Application Support\/Skype\/aidzis_skype\/main\.db/, @path
     end
   end
 end
