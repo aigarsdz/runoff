4033ea56544db8a2d22465c235c96af597de833d
Aigars Dzerviniks <dzerviniks.aigars@outlook.com>
2014-06-22 13:10:39 +0300

    Use MiniTest::Unit instead of MiniTest::Spec

--- test/command_all_test.rb
+++ test/command_all_test.rb
@@ -3,8 +3,8 @@ require 'runoff/commandline/all'
 require 'colorize'
 require 'fileutils'
 
-describe Runoff::Commandline::All, 'Export all chats' do
-  before do
+class CommandAllTest < MiniTest::Unit::TestCase
+  def setup
     @current_directory      = File.expand_path(File.dirname(__FILE__))
 
     @default_output_dir     = "#@current_directory/data"
@@ -12,61 +12,7 @@ describe Runoff::Commandline::All, 'Export all chats' do
     @expected_message       = 'Exporting...'.colorize(:green) + "\n" + 'Finished.'.colorize(:green) + "\n"
   end
 
-  it 'must raise a ArgumentError if called with an empty list of arguments' do
-    command = Runoff::Commandline::All.new( archive: true )
-
-    -> { -> { command.execute [] }.must_output 'Exporting...' }.must_raise ArgumentError
-  end
-
-  it 'must export all chat history as a Zip archive and save it in the default location' do
-    command          = Runoff::Commandline::All.new( archive: true )
-    ENV['HOME']      = @default_output_dir
-
-    Runoff::Location.stub :default_skype_data_location, @test_database_location do
-      -> { command.execute ['white_mike'] }.must_output @expected_message
-    end
-
-    Dir["#@default_output_dir/*.zip"].count.must_equal 1
-  end
-
-  it 'must export all chat history as a folder in the default location if a --no-archive option is provided' do
-    command          = Runoff::Commandline::All.new( archive: false )
-    ENV['HOME']      = @default_output_dir
-
-    Runoff::Location.stub :default_skype_data_location, @test_database_location do
-      -> { command.execute ['white_mike'] }.must_output @expected_message
-    end
-
-    Dir["#@default_output_dir/skype_chat_history"].count.must_equal 1
-  end
-
-  it 'must export all chat history as a Zip archive from a custom location and save it in the default location' do
-    test_custom_database_location = "#@current_directory/data/custom_location/test_main.db"
-
-    command          = Runoff::Commandline::All.new( archive: true, from: test_custom_database_location )
-    ENV['HOME']      = @default_output_dir
-
-    Runoff::Location.stub :default_skype_data_location, @test_database_location do
-      -> { command.execute ['white_mike'] }.must_output @expected_message
-    end
-
-    Dir["#@default_output_dir/*.zip"].count.must_equal 1
-  end
-
-  it 'must export all chat history as a Zip archive and save it in a custom location' do
-    custom_output_location = "#@current_directory/data/custom_output_location"
-
-    command          = Runoff::Commandline::All.new( archive: true, destination: custom_output_location )
-    ENV['HOME']      = @default_output_dir
-
-    Runoff::Location.stub :default_skype_data_location, @test_database_location do
-      -> { command.execute ['white_mike'] }.must_output @expected_message
-    end
-
-    Dir["#@default_output_dir/custom_output_location/*.zip"].count.must_equal 1
-  end
-
-  after do
+  def teardown
     Dir["#@default_output_dir/*"].each do |f|
       if File.extname(f) == '.zip'
         File.delete f
@@ -74,5 +20,71 @@ describe Runoff::Commandline::All, 'Export all chats' do
         FileUtils.rm_rf f
       end
     end
+  end
+
+  def test_it_raises_a_ArgumentError_if_called_with_an_empty_list_of_arguments
+    command = Runoff::Commandline::All.new( archive: true )
+
+    assert_raises ArgumentError do
+      assert_output 'Exporting...' do
+        command.execute []
+      end
+    end
+  end
+
+  def test_it_exports_all_chat_history_as_a_zip_archive_and_saves_it_in_the_default_location
+    command     = Runoff::Commandline::All.new( archive: true )
+    ENV['HOME'] = @default_output_dir
+
+    Runoff::Location.stub :default_skype_data_location, @test_database_location do
+      assert_output @expected_message do
+        command.execute ['white_mike']
+      end
+    end
+
+    assert_equal 1, Dir["#@default_output_dir/*.zip"].count
+  end
+
+  def test_it_exports_all_chat_history_as_a_folder_in_the_default_location_if_a_no_archive_option_is_set
+    command     = Runoff::Commandline::All.new( archive: false )
+    ENV['HOME'] = @default_output_dir
+
+    Runoff::Location.stub :default_skype_data_location, @test_database_location do
+      assert_output @expected_message do
+        command.execute ['white_mike']
+      end
+    end
+
+    assert_equal 1, Dir["#@default_output_dir/skype_chat_history"].count;
+  end
+
+  def test_it_exports_all_chat_history_as_a_zip_archive_from_a_custom_location_and_saves_it_in_the_default_location
+    test_custom_database_location = "#@current_directory/data/custom_location/test_main.db"
+
+    command     = Runoff::Commandline::All.new( archive: true, from: test_custom_database_location )
+    ENV['HOME'] = @default_output_dir
+
+    Runoff::Location.stub :default_skype_data_location, @test_database_location do
+      assert_output @expected_message do
+        command.execute ['white_mike']
+      end
+    end
+
+    assert_equal 1, Dir["#@default_output_dir/*.zip"].count
+  end
+
+  def test_it_exports_all_chat_history_as_a_zip_archive_and_saves_it_in_a_custom_location
+    custom_output_location = "#@current_directory/data/custom_output_location"
+
+    command          = Runoff::Commandline::All.new( archive: true, destination: custom_output_location )
+    ENV['HOME']      = @default_output_dir
+
+    Runoff::Location.stub :default_skype_data_location, @test_database_location do
+      assert_output @expected_message do
+        command.execute ['white_mike']
+      end
+    end
+
+    assert_equal 1, Dir["#@default_output_dir/custom_output_location/*.zip"].count
   end
 end
